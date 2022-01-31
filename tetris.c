35a45423c5016dd96291a8fd44f1fbfb33995164
tuopii <79019017+tuopii@users.noreply.github.com>
2022-01-31 18:52:29 +0200

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -1,6 +1,6 @@
 #include "tetris.h"
 #include <stdio.h>
-void paivita(char ruudukko[20][10]) {
+void update(char grid[20][10]) {
     int i;
     int j;
     int l;
@@ -8,14 +8,14 @@ void paivita(char ruudukko[20][10]) {
     char apu[10];
     for (i = 1; i < 20; ++i) {
         for (j = 0; j < 10; ++j) {
-            if (ruudukko[i][j] == ' ') {
+            if (grid[i][j] == ' ') {
                 ++lkm;
             }
         }
         if (lkm == 0) {
             for (l = i; l >= 2; --l) {
                 for (j = 0; j < 10; ++j) {
-                    apu[j] = ruudukko[l - 1][j];
+                    apu[j] = grid[l - 1][j];
                     ruudukko[l][j] = apu[j];
                     ruudukko[l - 1][j] = ' ';
                 }
