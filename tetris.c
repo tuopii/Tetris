f49f44709ac158daef5beae9e3bff620a8addedd
tuopii <79019017+tuopii@users.noreply.github.com>
2022-01-31 18:53:49 +0200

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -4,23 +4,23 @@ void update(char grid[20][10]) {
     int i;
     int j;
     int l;
-    int lkm = 0;
-    char apu[10];
+    int count = 0;
+    char tmp[10];
     for (i = 1; i < 20; ++i) {
         for (j = 0; j < 10; ++j) {
             if (grid[i][j] == ' ') {
-                ++lkm;
+                ++count;
             }
         }
-        if (lkm == 0) {
+        if (count == 0) {
             for (l = i; l >= 2; --l) {
                 for (j = 0; j < 10; ++j) {
-                    apu[j] = grid[l - 1][j];
-                    ruudukko[l][j] = apu[j];
-                    ruudukko[l - 1][j] = ' ';
+                    tmp[j] = grid[l - 1][j];
+                    grid[l][j] = tmp[j];
+                    grid[l - 1][j] = ' ';
                 }
             }
         }
-        lkm = 0;
+        count = 0;
     }
 }
